367f165a0ba53ce13bfcfaf62dfc656ed49bb648
palak2001 <iit2018117@iiita.ac.in>
2019-07-11 23:04:36 +0530

    CRUD operations with valdation :p

--- index.js
+++ index.js
@@ -1,54 +1,86 @@
+const startupDebug= require(debug)()
 const express= require(express());
 const app= express();
 const port= process.env.PORT||3000;
 
+const villas=[
+    {
+        id: 1,
+        address: 'Mount Everest'
+    },
+    {
+        id: 2,
+        address: 'Black Hole'
+    },
+    {
+        id: 3,
+        address: 'Bermuda Triangle'
+    },
+    {
+        id: 4,
+        address: 'Dead Sea'
+    }
+];
+
 app.use(express.json());
 //receiving the home page
 app.get('/', (req,res) => {
-    res.write("Welcome to the home page");
+    return res.write("Welcome to the home page");
 });
 
 //viewing the list of villas
 app.get('/api/villas', (req,res)=>{
     if(!villas)
-    res.send(404);
-    else
-    res.send(villas);
+    return res.send(404);
+    return res.send(villas);
 });
 
 //viewing a particular villa wth given id
 app.get('.api/villas/:id', (req,res)=>{
     const villa= villas.find(v=> v.id===parseInt(villas.params.id));
     if(!villa)
-    res.send(404);
-    else
-    res.send(villa);
+    return res.send(404);
+    return res.send(villa);
 });
 
 //creating the new villa
 app.post('/api/villas', (req,res)=>{
+    const {error}= validation(req.body);   
+    if (error) return res.status(400).send(error.details[0].message);
     const villa={
         id: villas.length() +1 ,
-        address: 'Heaven'
+        address: req.body.address
     }
     villas.push(villa);
-    res.send(villa);
+    return res.send(villa);
 });
 
 //updating the value of a particular villa with given id
 app.put('/api/villas/:id', (req,res)=>{
     const villa= villas.find(v=> v.id===parseInt(villas.params.id));
     if(!villa)
-    res.send(404);
+    return res.send(404);
     villa.address=req.body();
-    res.send(villa);
+    return res.send(villa);
 });
 
 //deleting the villa info with given id
 app.delete('/api/villas/:id', (req,res)=>{
     const villa= villas.find(v=> v.id===parseInt(villas.params.id));
     if(!villa)
-    res.send(404);
+    return res.send(404);
     //delete
-    res.send(villas);
+    return res.send(villas);
 });
+
+//user info validation function
+function validation(villa)
+{
+    startupDebug('Validation the info by user....');
+    const schema={
+        address: Joi.string.min(3).required
+    };
+    return Joi.validate(villa, schema);
+}
+
+app.listen(port , ()=> startupDebug(`Listening to the port ${port}`));
